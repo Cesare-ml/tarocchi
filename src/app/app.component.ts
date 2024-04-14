a15ce129b7bb216c4f6602b1cd377299e33c222a
Mirko Lancerotto <mirko.lance@gmail.com>
2024-04-14 21:04:07 +0200

    add cover carte e gioco geometrico

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ moment.locale('it');
 
 import marsigliesi from '../assets/json/marsigliesi.json';
 
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -16,16 +18,52 @@ export class AppComponent {
 
   cartaScoperta = -1;
 
+  giocoGeometricoCartaPos;
+  giocoGeometricoNumLati = 6;
+
+  constructor() {
+    this.giocoGeometricoCartaPosInit();
+    this.giocoGeometricoCartaPosCalc();
+  }
+
   ngOnInit() {
     this.rimescolaCarte();
   }
 
-  rimescolaCarte() {
+  onGiocoGeometricoNumLatiChange() {
+    this.giocoGeometricoCartaPosCalc();
+  }
+
+  giocoGeometricoCartaPosInit() {
+    this.giocoGeometricoCartaPos = [];
+    for(var i=0; i<this.carte.length; i++) {
+      this.giocoGeometricoCartaPos[i] = [0,0,0]; //rotate,transform
+    }
+  }
+  giocoGeometricoCartaPosCalc() {
+    var zIndexMax = Math.trunc(this.carte.length/this.giocoGeometricoNumLati);
+    for(var i=0; i<this.carte.length; i++) {
+      var livello = Math.trunc(i/this.giocoGeometricoNumLati);
+      var zIndex = zIndexMax-Math.trunc((i/this.giocoGeometricoNumLati));
+      var lato = i%this.giocoGeometricoNumLati;
+      var deg = 360*lato/this.giocoGeometricoNumLati
+      this.giocoGeometricoCartaPos[i] = [deg,livello,zIndex]; //degOffs si o no,rotate,transform,zindex
+    }
+  }
+
+  async rimescolaCarte() {
+    this.giocoGeometricoCartaPosInit();
+    await delay(300);
+
     this.cartaScoperta = -1;
     for (let i = this.carte.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * this.carte.length);
       [this.carte[i], this.carte[j]] = [this.carte[j], this.carte[i]];
     }
+    
+    this.giocoGeometricoCartaPosInit();
+    await delay(300);
+    this.giocoGeometricoCartaPosCalc();
   }
 
   onCartaClick(i) {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'; // <== add the imports!
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
 
@@ -9,6 +10,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
     FormsModule,
     ReactiveFormsModule
